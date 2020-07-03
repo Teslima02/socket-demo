83e98dd5298dcfc265e00c0b5b314558793c472a
Teslim <teslimafeez@gmail.com>
2020-07-03 15:35:05 +0100

    updated files

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,8 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+// var socket = io.connect('https://api.waya-ng.com', {query: {access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWYzMzI4MjVhYzVmMTA4MDNhNmQ5Y2YiLCJpYXQiOjE1OTMwOTQwMjZ9.8TWTTuagWPinCIWVXxfv_5YXk3eTwsCNpZp7kKqSvqU"}});
+// var socket = io.connect('http://apises.waya-ng.com', {query: {access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWYyMjcwYjYzMzA2OTBmZGEyNjMxOWYiLCJpYXQiOjE1OTMwOTQxOTZ9.XsxAK5lmuIiccSI5KPGmaBJ1_tWZ6wf8kdR74u7D-to"}});
+// var socket = io.connect('http://3.21.186.123', {query: {access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWYyMjcwYjYzMzA2OTBmZGEyNjMxOWYiLCJpYXQiOjE1OTMwOTQxOTZ9.XsxAK5lmuIiccSI5KPGmaBJ1_tWZ6wf8kdR74u7D-to"}});
+var socket = io.connect('http://api.waya-ng.com', {query: {access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWY5OTJjZGViYzVlYjEwNjBkZDY5MTgiLCJpYXQiOjE1OTM3NzgwNDZ9.2d-w_lSkfKX5sukIgQkEypzvrWg0r3mRZOIU06IgdqM"}});
 
 // Query DOM
 var message = document.getElementById('message'),
@@ -8,18 +11,47 @@ var message = document.getElementById('message'),
       output = document.getElementById('output'),
       feedback = document.getElementById('feedback');
 
+
+var message1 = {
+        _id: '5ef2270b6330690fda26319f',
+        userId2: '5ef07e086330690fda26318f',
+        message: 'Hello Good morning',
+        messageQuote: '',
+        forwardMessage: false,
+    }
+
+    var deleteMessage = {
+        messageId: '5ef269436330690fda2631b6',
+        userId1: '5ef2270b6330690fda26319f',
+        userId2: '5ef07e086330690fda26318f',
+        chatId: '5ef07e086330690fda26318f5ef2270b6330690fda26319f',
+    }
+
 // Emit events
 btn.addEventListener('click', function(){
-    socket.emit('chat', {
-        message: message.value,
-        handle: handle.value
-    });
-    message.value = "";
+    // post message
+    // socket.emit('/api/chat/post',message1);
+
+    // delete message
+    socket.emit('/api/chat/delete',deleteMessage);
 });
 
 message.addEventListener('keypress', function(){
     socket.emit('typing', handle.value);
 })
+
+socket.on('/api/chat/post', function(data){
+    console.log(data, 'data');
+});
+
+socket.on('/api/chat', function(data){
+    console.log(data, 'data');
+});
+
+// get deleted message
+socket.on('/api/chat/delete', function(data){
+    console.log(data, 'data')
+});
 
 // Listen for events
 socket.on('chat', function(data){
